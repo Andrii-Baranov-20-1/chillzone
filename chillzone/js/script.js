a2bdc29ce5173f8f2e44319bb257e629fda76a20
Andrii-Baranov-20-1 <168442451+Andrii-Baranov-20-1@users.noreply.github.com>
2024-05-20 23:01:24 +0300

    test-3
    test-3

--- chillzone/js/script.js
+++ chillzone/js/script.js
@@ -9,16 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
         reset: true
     });
     /*--- index.html ---*/
-    sr.reveal('.section-main-text', { delay: 200, origin: 'top' });
-    sr.reveal('.section-button', { delay: 300, origin: 'bottom' });
-    sr.reveal('.main-second-text', { delay: 200, origin: 'bottom' });
+    sr.reveal('.section-menu', { delay: 200, origin: 'top', reset: false });
+    sr.reveal('.first-button', { delay: 300, origin: 'bottom', reset: false });
+    sr.reveal('.second-menu-text', { delay: 200, origin: 'bottom', reset: false });
     /*--- about.html ---*/
-    sr.reveal('.info-left h1', { delay: 200, origin: 'top' });
-    sr.reveal('.info-left p', { delay: 300, origin: 'bottom' });
-    sr.reveal('.info-right h1', { delay: 200, origin: 'top' });
-    sr.reveal('.info-right p', { delay: 300, origin: 'bottom' });
+    sr.reveal('.info-left h1', { delay: 200, origin: 'top', reset: false });
+    sr.reveal('.info-left p', { delay: 300, origin: 'bottom', reset: false });
+    sr.reveal('.info-right h1', { delay: 200, origin: 'top', reset: false });
+    sr.reveal('.info-right p', { delay: 300, origin: 'bottom', reset: false });
 
-    sr.reveal('.testimonials h2', { delay: 200, origin: 'top' });
-    sr.reveal('.testimonial p', { delay: 600, origin: 'bottom' });
+    sr.reveal('.testimonials h2', { delay: 200, origin: 'top', reset: false });
+    sr.reveal('.testimonial p', { delay: 600, origin: 'bottom', reset: false });
 
 });
